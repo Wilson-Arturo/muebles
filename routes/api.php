87ab09e8283bf5f8203377fb23d4752f178f9e24
Wilson Arturo Rojas Tellez <al222010001@gmail.com>
2022-04-08 06:50:49 -0500

    traits request

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< HEAD
+
 /* typeuser */
 Route::resource('typeuser', TypeUserController::class);
 
@@ -32,10 +32,3 @@ Route::resource('product', UserController::class);
 
 /* details */
 Route::resource('detail', UserController::class);
-=======
-// Route::resource(name: 'usuario', controller: 'UsuarioController');
-Route::get('/productos', 'App\Http\Controllers\ProductoController@index');//mostrar todos los registros
-Route::post('/productos', 'App\Http\Controllers\ProductoController@store');//crear un registro
-Route::put('/productos/{id_productos}', 'App\Http\Controllers\ProductoController@update');//actualizar
-Route::delete('/productos/{id_productos}', 'App\Http\Controllers\ProductoController@destroy');//borrar
->>>>>>> 3269c56f136b3cd772195605d1910f8e7c55ab9e
